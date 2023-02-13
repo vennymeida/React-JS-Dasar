d133f64ba0c9edcdb40301428826beec057e8bbc
vennymeida <vnymeida@gmail.com>
2023-02-13 10:25:35 +0700

    Client Side Rendering

--- index.js
+++ index.js
@@ -4,18 +4,48 @@ const root = document.getElementById("root");
 
 linkHome.onclick = function(event){
     event.preventDefault();
-    root.textContent = "Welcome to Home";
+    const homeScreen = HomeScreen();
+    root.innerHTML = "";
+    root.append(homeScreen);
     history.pushState(null, "", event.target.href);
 }; 
 
 linkAbout.onclick = function(event){
     event.preventDefault();
-    root.textContent = "Welcome to About";
+    const aboutScreen = AboutScreen();
+    root.innerHTML = "";
+    root.append(aboutScreen);
     history.pushState(null, "", event.target.href);
-}; 
+};
+
+function AboutScreen(){
+    const text = document.createElement("p");
+    text.textContent = "Welcome to About";
+    return text;
+}
+
+function HomeScreen(){
+    const textPreview = document.createElement("p");
+
+    const input = document.createElement("input");
+    input.oninput = function(event){
+        textPreview.textContent = event.target.value;
+    };
+    input.placeholder = "Enter your name";
+
+    const div = document.createElement("div");
+    div.append(input);
+    div.append(textPreview);
+
+    return div;
+}
 
 if(location.hash == "#about"){
-    root.textContent = "Welcome to About";
+    const aboutScreen = AboutScreen();
+    root.innerHTML = "";
+    root.append(aboutScreen);
 } else if (location.hash == "#home"){
-    root.textContent = "Welcome to Home";
+    const homeScreen = HomeScreen();
+    root.innerHTML = "";
+    root.append(homeScreen);
 }
