572e774d73985495727503d1994c923219d68f8f
vennymeida <vnymeida@gmail.com>
2023-02-13 11:14:53 +0700

    Full Client Side Rendering

--- index.js
+++ index.js
@@ -1,39 +1,69 @@
-const linkHome = document.getElementById("link-home");
-const linkAbout = document.getElementById("link-about");
 const root = document.getElementById("root");
 
-linkHome.onclick = function(event){
-    event.preventDefault();
-    const homeScreen = HomeScreen();
-    root.innerHTML = "";
-    root.append(homeScreen);
-    history.pushState(null, "", event.target.href);
-}; 
+function Navbar(){
+    const linkHome = document.createElement("a");
+    linkHome.href = "#home";
+    linkHome.textContent = "Home";
+    linkHome.onclick = function(event){
+        event.preventDefault();
+        const homeScreen = HomeScreen();
+        root.innerHTML = "";
+        root.append(homeScreen);
+        history.pushState(null, "", event.target.href);
+    }; 
 
-linkAbout.onclick = function(event){
-    event.preventDefault();
-    const aboutScreen = AboutScreen();
-    root.innerHTML = "";
-    root.append(aboutScreen);
-    history.pushState(null, "", event.target.href);
-};
+    const linkAbout = document.createElement("a");
+    linkAbout.href = "#about";
+    linkAbout.textContent = "About";
+    linkAbout.onclick = function(event){
+        event.preventDefault();
+        const aboutScreen = AboutScreen();
+        root.innerHTML = "";
+        root.append(aboutScreen);
+        history.pushState(null, "", event.target.href);
+    }; 
+
+    const div = document.createElement("div");
+    div.append(linkHome);
+    div.append(linkAbout);
+
+    return div;
+}
 
 function AboutScreen(){
+    const linkHome = document.createElement("a");
+    linkHome.href = "#home";
+    linkHome.textContent = "Kembali ke Home";
+    linkHome.onclick = function(event){
+        event.preventDefault();
+        const homeScreen = HomeScreen();
+        root.innerHTML = "";
+        root.append(homeScreen);
+        history.pushState(null, "", event.target.href);
+    }; 
+
     const text = document.createElement("p");
     text.textContent = "Welcome to About";
-    return text;
+
+    const div = document.createElement("div");
+    div.append(linkHome);
+    div.append(text);
+    return div;
 }
 
 function HomeScreen(){
+    const navbar = Navbar();
+
     const textPreview = document.createElement("p");
 
     const input = document.createElement("input");
+    input.placeholder = "Enter your name";
     input.oninput = function(event){
         textPreview.textContent = event.target.value;
     };
-    input.placeholder = "Enter your name";
 
     const div = document.createElement("div");
+    div.append(navbar);
     div.append(input);
     div.append(textPreview);
 
